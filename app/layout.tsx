c56f12eaa4a9b3f9574f9508868d277815f9f55c
dan-evo-go <dson.email@gmail.com>
2025-04-07 21:13:16 +1000

    Create a not-found 404 page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Merriweather } from "next/font/google";
+import { Merriweather } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 
