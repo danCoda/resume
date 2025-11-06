ac2a6edca87894407cbf564cb9d11bc74be12805
dan-evo-go <dson.email@gmail.com>
2025-11-06 17:50:53 +1100

    Fix eslint error

--- app/components/dan-description.tsx
+++ app/components/dan-description.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/app/constants";
 import { Switch } from "@/app/components/ui/switch";
 
-export const DanDescription = () => {
+export const DanDescriptionOld = () => {
   const [isPassionate, setIsPassionate] = useState(true);
   const [isTransitioning, setIsTransitioning] = useState(false);
 
@@ -23,14 +23,14 @@ export const DanDescription = () => {
     <section className="space-y-3">
       <div className="flex justify-end items-center">
         <h2 className="text-xl font-semibold sr-only">About Me</h2>
-        {/* <label className={`cursor-pointer flex items-center text-blue-800`}>
+        <label className={`cursor-pointer flex items-center text-blue-800`}>
           <Switch checked={isPassionate} onCheckedChange={toggleType} />
           <span className="pl-3 italic">
             {isPassionate
               ? danDescriptionStyleLabelCopyText.relax
               : danDescriptionStyleLabelCopyText.showPassion}
           </span>
-        </label> */}
+        </label>
       </div>
       <div
         className={`space-y-3 transition-all duration-300 ${
@@ -38,9 +38,23 @@ export const DanDescription = () => {
         }`}
       >
         {homepageCopyText.description[
-          // isPassionate ? "passionate" : "default"
-          "default"
+          isPassionate ? "passionate" : "default"
         ].map((line, i) => (
+          <p key={i}>{line}</p>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export const DanDescription = () => {
+  return (
+    <section className="space-y-3">
+      <div className="flex justify-end items-center">
+        <h2 className="text-xl font-semibold sr-only">About Me</h2>
+      </div>
+      <div className={`space-y-3`}>
+        {homepageCopyText.description["default"].map((line, i) => (
           <p key={i}>{line}</p>
         ))}
       </div>
